e2c98833e046bb3c57d1268008f3d6e407b70ee1
Ahmet Efe Cetin <148224809+cetinefe@users.noreply.github.com>
2025-10-22 14:56:51 +0200

    escape string "/" issue

--- SNP_Exam_Generator.py
+++ SNP_Exam_Generator.py
@@ -154,8 +154,8 @@ def create_html_content(questions, new_exam_number):
                 // Highlight correct answers
                 correct.forEach(correctAns => {{
                     // Escape special characters in the correct answer
-                    const escapedCorrectAns = correctAns.replace(/([!"#$%&'()*+,.\/:;<=>?@[\\\]^`{{|}}~])/g, '\\$1');
-                    const correctLabel = questionDiv.querySelector(`label[for="q${{key}}_${{escapedCorrectAns.replace(/\\s/g, '_')}"]`);
+                    const escapedCorrectAns = correctAns.replace(/([!"#$%&'()*+,.\\/:;<=>?@[\\\]^`{{|}}~])/g, '\\$1');
+                    const correctLabel = questionDiv.querySelector(`label[for="q${{key}}_${{escapedCorrectAns.replace(/\\s/g, '_')}}"]`);
                     if (correctLabel) {{
                         correctLabel.parentElement.classList.add('correct-answer');
                     }}
