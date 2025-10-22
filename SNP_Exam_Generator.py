90c9e76ce6b7220c4ead02824aad9c39b3862c3c
Ahmet Efe Cetin <148224809+cetinefe@users.noreply.github.com>
2025-10-22 15:03:58 +0200

    "]" issues again

--- SNP_Exam_Generator.py
+++ SNP_Exam_Generator.py
@@ -154,7 +154,7 @@ def create_html_content(questions, new_exam_number):
                 // Highlight correct answers
                 correct.forEach(correctAns => {{
                     // Escape special characters in the correct answer
-                    const escapedCorrectAns = correctAns.replace(/([!"#$%&'()*+,.\\//:;<=>?@[\\\]^`{{|}}~])/g, '\\$1');
+                    const escapedCorrectAns = correctAns.replace(/([!"#$%&'()*+,.\\/:;<=>?@[\\]]^`{{|}}~])/g, '\\$1');
                     const correctLabel = questionDiv.querySelector(`label[for="q${{key}}_${{escapedCorrectAns.replace(/\\s/g, '_')}}"]`);
                     if (correctLabel) {{
                         correctLabel.parentElement.classList.add('correct-answer');
